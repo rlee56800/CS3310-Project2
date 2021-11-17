7e4912de79e4228b7d7f1ceb95d19ab0625df502
rlee56800 <78502232+rlee56800@users.noreply.github.com>
2021-11-17 02:11:41 -0800

    can read input

---
+++ 01Knapsack.py
@@ -0,0 +1,8 @@
+capacity = input("Please enter value for capacity: ")
+
+# open input.txt and take items
+file = open("input.txt", "r")
+print(file.read())
+file.close()
+
+
---
+++ FractionalKnapsack.py
@@ -0,0 +1,7 @@
+capacity = input("Please enter value for capacity: ")
+
+
+# open input.txt and take items
+file = open("input.txt", "r")
+print(file.read())
+file.close()
