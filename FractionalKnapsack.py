31fd26b2088367f80d125b80d24c1a3d50b256f1
rlee56800 <78502232+rlee56800@users.noreply.github.com>
2021-11-18 22:29:52 -0800

    fk works

--- FractionalKnapsack.py
+++ FractionalKnapsack.py
@@ -16,43 +16,46 @@ OUTPUT:
 # fills knapsack; enter index of largest p/w ratio 
 def fill(tot, cap, index):
     if weight[index] > cap:
-        print("weight > cap HHHH")
+        #print("weight > cap HHHH")
         return tot + (profitperunit[index] * cap), 0
     else:
-        print("cap < weight item:" + str(index))
+        #print("cap < weight item:" + str(index))
         profitperunit[index] = -1
         return tot + (profit[index]), cap - weight[index]
     
 
-#capacity = input("Please enter value for capacity: ")
-# optimally between 2 and 24, exclusive
-capacity = 20
+capacity = int(input("Please enter value for capacity (for this data set, optimally under 24): "))
+#capacity = 20
 total = 0
 
+with open('input.txt') as f:
+    items = int(next(f)) # read first line
+    profit = []
+    for x in next(f).split():
+        profit.append(int(x))
+    weight = []
+    for x in next(f).split():
+        weight.append(int(x))
+#print(profit)
+#print(weight)
+
 '''
-# open input.txt and take items
-file = open("input.txt", "r")
-print(file.read())
-file.close()
+# Until I figure out how to read from txt file
+items = 5
+profit = [16, 10, 18, 10, 6]
+weight = [2, 5, 6, 10, 1]
 '''
 
-# Until I figure out how to read from txt file
-items = 3#5
-profit = [25, 24, 15]#[16, 10, 18, 10, 6]
-weight = [18, 15, 10]#[2, 5, 6, 10, 1]
 
 profitperunit = []
 for i in range(items):
     profitperunit.append(profit[i]/weight[i])
 total = 0
-print(profitperunit)
+#print(profitperunit)
 
 while capacity != 0 and max(profitperunit) != -1:
-    #temp = fill(total, capacity, profitperunit.index(max(profitperunit)))
-    #total = total + temp[0]
-    #capacity = temp[1]
     total, capacity = fill(total, capacity, profitperunit.index(max(profitperunit)))
-    print(total)
-    print(capacity)
+    #print(total)
+    #print(capacity)
 
 print("Total: " + str(total))
