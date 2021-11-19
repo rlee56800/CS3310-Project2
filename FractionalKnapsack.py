1cb7a69ee50df8b08b0a9b82b2c77625cb30b80a
rlee56800 <78502232+rlee56800@users.noreply.github.com>
2021-11-19 03:12:59 -0800

    fk cleaned + commented

--- FractionalKnapsack.py
+++ FractionalKnapsack.py
@@ -13,28 +13,30 @@ OUTPUT:
     output considering knapsack capacity.
 '''
 
-# fills knapsack; enter index of largest p/w ratio 
+# fills knapsack; enter index of largest p/w ratio
+# returns total profit of items in knapsack and remaining capacity of bag
 def fill(tot, cap, index):
-    if weight[index] > cap:
-        #print("weight > cap HHHH")
+    if weight[index] > cap: # if there's no more room in the knapsack (split an item)
+        #print("weight > cap")
         return tot + (profitperunit[index] * cap), 0
-    else:
+    else: # if a whole item can fit in the knapsack
         #print("cap < weight item:" + str(index))
-        profitperunit[index] = -1
+        profitperunit[index] = -1 # item is used up
         return tot + (profit[index]), cap - weight[index]
     
 
 capacity = int(input("Please enter value for capacity (for this data set, optimally under 24): "))
-#capacity = 20
+#capacity = 20 # testing
 total = 0
+profit = []
+weight = []
+profitperunit = []
 
 with open('input.txt') as f:
-    items = int(next(f)) # read first line
-    profit = []
-    for x in next(f).split():
+    items = int(next(f)) # first number is amount of items
+    for x in next(f).split(): # first row is profit
         profit.append(int(x))
-    weight = []
-    for x in next(f).split():
+    for x in next(f).split(): # second row is weights
         weight.append(int(x))
 #print(profit)
 #print(weight)
@@ -47,13 +49,11 @@ weight = [2, 5, 6, 10, 1]
 '''
 
 
-profitperunit = []
 for i in range(items):
     profitperunit.append(profit[i]/weight[i])
-total = 0
 #print(profitperunit)
 
-while capacity != 0 and max(profitperunit) != -1:
+while capacity != 0 and max(profitperunit) != -1: # while bag has space and there are items left
     total, capacity = fill(total, capacity, profitperunit.index(max(profitperunit)))
     #print(total)
     #print(capacity)
