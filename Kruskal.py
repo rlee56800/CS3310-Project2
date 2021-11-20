087c479761a8d998307d386d1afe7288d983c574
rlee56800 <78502232+rlee56800@users.noreply.github.com>
2021-11-19 18:52:24 -0800

    k attempt (fail)

--- Kruskal.py
+++ Kruskal.py
@@ -14,11 +14,28 @@ OUTPUT:
     Print the list of all the edges in MST and the total cost of MST.
 '''
 
+class Edge:
+    def __init__(self, p1, p2, weight):
+        self.p1 = p1
+        self.p2 = p2
+        self.weight = weight
+
 def kruskal():
     sorted_arr = sorted(arr, key=lambda x: x[2])
     F = []
-    while len(F) > len(arr):
-        print()
+    isAValue = True
+    counter = 0
+    while len(F) > len(arr) - 1:
+        while isAValue:
+            if sorted_arr[counter] == -1:
+                counter += 1
+            else:
+                e = arr[counter][2]
+                isAValue = False
+        i, j = arr[counter][0], arr[counter][1]
+        p = 0
+        # edge (not -1)
+        # 
 
 
 arr = []
