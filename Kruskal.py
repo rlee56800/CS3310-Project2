14035d24cdfb916c0a8aaffccc41c62cd52cc31a
rlee56800 <78502232+rlee56800@users.noreply.github.com>
2021-11-19 20:50:37 -0800

    k idk

--- Kruskal.py
+++ Kruskal.py
@@ -15,23 +15,22 @@ OUTPUT:
 '''
 
 class Graph:
-    def __init__(self, vertices):
-        self.vertices = vertices
-        self.edges = []
-
-    def add(self, arr):
-        self.edges.append(arr)
-
-    # Search function
-
-    def find(self, parent, i):
+    def __init__(self, vertex):
+        self.vertex = vertex
+        self.graph = []
+ 
+    def add(self, u, v, w):
+        self.graph.append([u, v, w])
+ 
+ 
+    def search(self, parent, i):
         if parent[i] == i:
             return i
-        return self.find(parent, parent[i])
-
+        return self.search(parent, parent[i])
+ 
     def union(self, parent, rank, x, y):
-        xroot = self.find(parent, x)
-        yroot = self.find(parent, y)
+        xroot = self.search(parent, x)
+        yroot = self.search(parent, y)
         if rank[xroot] < rank[yroot]:
             parent[xroot] = yroot
         elif rank[xroot] > rank[yroot]:
@@ -39,36 +38,52 @@ class Graph:
         else:
             parent[yroot] = xroot
             rank[xroot] += 1
-
-    #  Applying Kruskal algorithm
+ 
+  
     def kruskal(self):
         result = []
         i, e = 0, 0
-        self.edges = sorted(self.edges, key=lambda item: item[2])
+        self.graph = sorted(self.graph, key=lambda item: item[2])
         parent = []
         rank = []
-        for node in range(self.vertices):
+        for node in range(self.vertex):
             parent.append(node)
             rank.append(0)
-        while e < self.vertices - 1:
-            u, v, w = self.edges[i]
+        while e < self.vertex - 1:
+            u, v, w = self.graph[i]
             i = i + 1
-            x = self.find(parent, u)
-            y = self.find(parent, v)
+            x = self.search(parent, u)
+            y = self.search(parent, v)
             if x != y:
                 e = e + 1
                 result.append([u, v, w])
                 self.union(parent, rank, x, y)
+        
+        print("Edges:")
+        mst = 0
         for u, v, weight in result:
-            print(str(u) + " - " + str(v) + ": " + str(weight))
+            print("  (" + str(u) + ", " + str(v) + ") = " + str(weight))
+            mst += weight
+        print("MST total cost: " + str(mst))        
 
-
+'''
 with open('kruskal_input.txt') as f:
-    curGraph = Graph(int(next(f)))
+    g = Graph(int(next(f)))
     for line in f: # read rest of lines
         arr = []
-        for x in range(10):# next(f).split():
-            arr.append(int(x))
-        curGraph.add(arr)
+        arr = [int(x) for x in line.split()]
+        g.add(arr[0], arr[1], arr[2])
 
-curGraph.kruskal()
+g.kruskal()'''
+
+
+g = Graph(5)
+arr = [0, 1, 8]
+g.add(arr[0], arr[1], arr[2])
+g.add(0, 2, 5)
+g.add(1, 2, 9)
+g.add(1, 3, 11)
+g.add(2, 3, 15)
+g.add(2, 4, 10)
+g.add(3, 4, 7)
+g.kruskal()
